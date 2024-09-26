65e0a7ce904a74c1b071f807c56c7ec4d66641f1
kuppam kamalesh <124693521+kuppamkamalesh@users.noreply.github.com>
2024-09-26 17:06:30 +0530

    Add files via upload

--- src/App-v2.js
+++ src/App-v2.js
@@ -104,7 +104,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            ` http://www.omdbapi.com/?apikey=${KEY}&s=${query} `,
+            ` https://www.omdbapi.com/?apikey=${KEY}&s=${query} `,
             { signal: controller.signal }
           );
 
@@ -356,7 +356,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setisLoading(true);
         const res = await fetch(
-          ` http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          ` https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         console.log(data);
